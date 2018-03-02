45cad8216446b161ddc2fb48283d734addf2af36
minamicr <minamicr@yahoo.com.br>
2018-03-01 21:39:20 -0300

    Add files via upload

---
+++ src/todo/todo.jsx
@@ -0,0 +1,50 @@
+import React, {Component } from 'react'
+import axios from 'axios'
+
+import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
+
+export default class Todo extends Component {
+    constructor (props){
+        super(props)
+        this.state = {description: '', list: []}
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh
+    }
+
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    }
+    
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value})
+    }
+    
+    handleAdd(){
+        const description = this.state.description
+        axios.post(URL, {description})
+            .then(resp => this.refresh())
+    }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
+    render() {
+        return (
+            <div>
+                <PageHeader name="todo" small="cadastro"/>
+                <TodoForm description={this.state.description}
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange} />
+                <TodoList list={this.state.list}/>
+            </div>
+        )
+    }
+}
